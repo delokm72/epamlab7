bd69f1b64757c97f0e8b055e7150188b45b2ab2a
Oleksandr Muravskyi <oleksandr.muravskyi@interactiv4.com>
2026-07-28 17:31:20 +0300

    descriptions mock

--- task07/modules/cdn/variables.tf
+++ task07/modules/cdn/variables.tf
@@ -1,25 +1,31 @@
 variable "blob_hostname" {
+  description = "..."
   type = string
 }
 
 variable "location" {
+  description = "..."
   type = string
 }
 
 variable "resource_group_name" {
+  description = "..."
   type = string
 }
 
 variable "profile_name" {
+  description = "..."
   type = string
 }
 
 variable "endpoint_name" {
+  description = "..."
   type = string
 }
 
 variable "origin_group_name" {
   type = string
+  description = "..."
 }
 
 variable "origin_name" {
